a673397331d43e7c23cacc51608f579cd5a3a752
Stefan Herpich <s10h@posteo.de>
2024-06-09 10:51:10 +0200

    fix: add night mode service to module

--- src/fan/fan.module.ts
+++ src/fan/fan.module.ts
@@ -3,10 +3,11 @@ import { FanController } from "./fan.controller";
 import { FanService } from "./fan.service";
 import { RfCommunicationModule } from "src/rf-communication/rf-communication.module";
 import { StorageModule } from "src/storage/storage.module";
+import { NightModeService } from "./night-mode-service";
 
 @Module({
   imports: [RfCommunicationModule, StorageModule],
   controllers: [FanController],
-  providers: [FanService],
+  providers: [FanService, NightModeService],
 })
 export class FanModule {}
